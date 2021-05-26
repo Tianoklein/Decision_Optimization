98feca50880c08a91ee76e78674ad816001ec801
PAULO CRISTIANO KLEIN <tianoklein@hotmail.com>
2021-05-25 21:21:42 -0300

    Update app.py

--- app.py
+++ app.py
@@ -9,30 +9,6 @@ from pyomo.opt import SolverFactory
 from gsheetsdb import connect
 
 
-st.subheader("IFRAME")
-
-html_temp = '''
-<iframe src="https://docs.google.com/spreadsheets/d/e/2PACX-1vQygBicNaTXHGVFdYbK9RooQlexyw2qoe0RDnVv7lh7JvWvwhYz_aB3ARX8s38U96IfPXDZvYCUgKlG/pubhtml?widget=true&amp;headers=false"></iframe>
-'''
-components.html(html_temp)   
-
-conn = connect()
-# Perform SQL query on the Google Sheet.
-# Uses st.cache to only rerun when the query changes or after 10 min.
-@st.cache(ttl=600)
-def run_query(query):
-    rows = conn.execute(query, headers=1)
-    return rows
-
-sheet_url = st.secrets["public_gsheets_url"]
-rows = run_query(f'SELECT * FROM "{sheet_url}"')
-
-# Print results.
-for row in rows:
-    st.write(f"{row.name} has a :{row.pet}:")
-
-
-
 st.title("Prescriptive Analitics")
 st.subheader("Decision Optimization")
 st.write(pyomo.version.version_info)
