e42d30a68d104d95be0920371e6420f0daf8cb9e
PAULO CRISTIANO KLEIN <tianoklein@hotmail.com>
2021-06-22 16:17:54 -0300

    glpk

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ y = model.y
 model.obj = pyo.Objective(expr = sum(x[i]+y[i] for i in range(3)))
 model.c = pyo.Constraint(expr = x[0]>=3)
 
-st.write(model.x.pprint())
+st.write(model.x.pprint('glpk'))
 opt = SolverFactory()
 results = opt.solve(model, tee=True)
 st.write(results)
